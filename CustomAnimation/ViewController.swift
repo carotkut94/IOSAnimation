b99423ad09efa258931e5cf03f8390fcc1cd8e5e
Death Code <deathcode@Deaths-MacBook-Pro.local>
2017-12-10 22:31:20 +0530

    progress added and capped

--- CustomAnimation/ViewController.swift
+++ CustomAnimation/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let shapeLayer = CAShapeLayer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // we will be drawing a circle first using CAShapeLayer
+        
+        
+        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        shapeLayer.path = circularPath.cgPath
+        
+        shapeLayer.strokeColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        shapeLayer.strokeEnd = 0
+        shapeLayer.lineWidth = 10
+        shapeLayer.lineCap = kCALineCapRound
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        
+        self.view.layer.addSublayer(shapeLayer)
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(gestureDetcted)))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc private func gestureDetcted(){
+        print("Clicked")
+        
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        basicAnimation.fillMode = kCAFillModeForwards
+        basicAnimation.isRemovedOnCompletion = false
+        shapeLayer.add(basicAnimation, forKey: "simpleAnimation")
     }
-
 
 }
 
