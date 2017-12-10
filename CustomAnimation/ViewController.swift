519ce8a2c95679b5a466fe27685eb247ffdab202
Death Code <deathcode@Deaths-MacBook-Pro.local>
2017-12-10 22:37:25 +0530

    Added complete animation procedure for layer and track

--- CustomAnimation/ViewController.swift
+++ CustomAnimation/ViewController.swift
@@ -16,18 +16,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         // we will be drawing a circle first using CAShapeLayer
         
+        // Add a track on which the progress follows
         
         let center = view.center
         let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
-        shapeLayer.path = circularPath.cgPath
+        let trackLayer = CAShapeLayer()
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         
-        shapeLayer.strokeColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        trackLayer.path = circularPath.cgPath
+        trackLayer.strokeColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        trackLayer.lineWidth = 10
+        trackLayer.lineCap = kCALineCapRound
+        trackLayer.fillColor = UIColor.clear.cgColor
+        view.layer.addSublayer(trackLayer)
+    
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
         shapeLayer.strokeEnd = 0
         shapeLayer.lineWidth = 10
         shapeLayer.lineCap = kCALineCapRound
         shapeLayer.fillColor = UIColor.clear.cgColor
-        
-        self.view.layer.addSublayer(shapeLayer)
+        view.layer.addSublayer(shapeLayer)
         
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(gestureDetcted)))
     }
